9679bf2269c20bccdd4753d6ecca5766d98f5956
devksh930 <ksh930@icloud.com>
2022-07-20 22:22:07 +0900

    Initial commit

---
+++ src/main/java/me/devksh930/orderapi/OrderApiApplication.java
@@ -0,0 +1,13 @@
+package me.devksh930.orderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/devksh930/orderapi/OrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.devksh930.orderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
